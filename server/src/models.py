e67ffa9368676045d351a0685ce13a125b2ce20c
lizardfish0 <97480684+lizardfish0@users.noreply.github.com>
2023-08-26 17:32:25 -0600

    Use pydantic models.

--- server/src/models.py
+++ server/src/models.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from pydantic import BaseModel
 
-@dataclass
-class Movie:
+
+class Movie(BaseModel):
     id: int
     title: str
     year: int
@@ -11,46 +12,57 @@ class Movie:
     folderPath: str
     tmdbId: Optional[int] = None
     imdbId: Optional[str] = None
+    overview: Optional[str] = None
 
 
-@dataclass
-class RemoteMovie:
+class RemoteMovie(BaseModel):
     tmdbId: int
     imdbId: str
     title: str
     year: int
 
 
-@dataclass
-class MovieFile:
+class MovieFile(BaseModel):
     id: int
     relativePath: str
     path: str
     quality: str
     qualityVersion: int
     releaseGroup: str
-    sceneName: str
     indexerFlags: str
     size: int
+    dateAdded: Optional[str] = None
+    mediaInfo: Optional[dict] = None
+    sceneName: Optional[str] = None
 
 
-@dataclass
-class Release:
-    quality: str
-    qualityVersion: int
-    releaseGroup: str
-    releaseTitle: str
-    indexer: str
-    size: int
+class Release(BaseModel):
+    quality: Optional[str] = None
+    qualityVersion: Optional[int] = None
+    releaseGroup: Optional[str] = None
+    releaseTitle: Optional[str] = None
+    indexer: Optional[str] = None
+    size: Optional[int] = None
+    customFormatScore: Optional[int] = None
+
+
+class CustomFormat(BaseModel):
+    id: int
+    name: str
+
+
+class CustomFormatInfo(BaseModel):
+    customFormats: list[CustomFormat]
     customFormatScore: int
 
 
-@dataclass
-class RadarrWebhook:
+class RadarrWebhook(BaseModel):
     movie: Movie
     remoteMovie: RemoteMovie
     instanceName: str
     eventType: str
+    customFormatInfo: Optional[CustomFormatInfo] = None
     isUpgrade: Optional[bool] = None
     movieFile: Optional[MovieFile] = None
     release: Optional[Release] = None
+    deletedFiles: Optional[list[MovieFile]] = None
